d635cfe59324a13eb17b338994ef0f71ede43aba
GeorgII <>
2024-03-23 01:59:55 +0300

    import my config

---
+++ schemes/dark.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#151515", /* black */
+	"#ac4142", /* red */
+	"#90a959", /* green */
+	"#f4bf75", /* yellow */
+	"#6a9fb5", /* blue */
+	"#aa759f", /* magenta */
+	"#75b5aa", /* cyan */
+	"#d0d0d0", /* white */
+
+	/* 8 bright colors */
+	"#505050", /* black */
+	"#ac4142", /* red */
+	"#90a959", /* green */
+	"#f4bf75", /* yellow */
+	"#6a9fb5", /* blue */
+	"#aa759f", /* magenta */
+	"#75b5aa", /* cyan */
+	"#f5f5f5", /* white */
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"#d0d0d0", /* default foreground colour */
+	"#151515", /* default background colour */
+};
